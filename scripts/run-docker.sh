7cdee850d6c11b662af17c44b6915a6563b53613
newstarter1997 <2218244114@qq.com>
2026-03-16 16:29:00 +0800

    Update run-docker.sh
    add tag android ndk r25c

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -57,7 +57,7 @@ else
 	VOLUME=$REPOROOT:$CONTAINER_HOME_DIR/termux-packages
 fi
 
-: ${TERMUX_BUILDER_IMAGE_NAME:=ghcr.io/termux/package-builder}
+: ${TERMUX_BUILDER_IMAGE_NAME:=ghcr.io/termux/package-builder:r25c}
 : ${CONTAINER_NAME:=termux-package-builder}
 
 USER=builder
